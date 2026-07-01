b9f2429e94ff883ad3a8182a2dd8aad28fde68b9
STORM533 <sakshamkr2006@gmail.com>
2026-07-02 00:24:25 +0530

    routing done between pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router";
 function Navbar() {
-    
+    return (
+        <div>
+            <Link to ="shoppage">SHOP  </Link>
+
+            <Link to ="Homepage">HOME  </Link>
+
+            <Link to ="Cartpage">CART  </Link>
+        </div>
+
+    );
 }
+export default Navbar;
